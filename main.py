8a6d36bcc9a067eccdd427b684db0b2fe38ca79d
RainyKnight <stoltmanlucas@gmail.com>
2022-05-24 15:26:30 -0700

    Basic todo flow

--- main.py
+++ main.py
@@ -4,4 +4,18 @@
 # CSS 340 A, Dimpsey
 # Version: 1.0
 
-print("Hello World!")
+from accounts import Accounts
+
+# 1) The program will read in a string of transactions from a file into an in-memory queue.
+# TODO read input file
+
+# TODO add to queue
+
+# 2) The program will next read from the queue and process the transactions in order.
+# TODO while queue !empty
+#   process first command
+
+# TODO When the queue has been
+#  depleted the program will print
+#  out all open accounts and balances
+#  in those accounts.
