ca84f5cb9c00e10d67214fa7b8052f18a89a20f8
RainyKnight <stoltmanlucas@gmail.com>
2022-05-25 01:06:02 -0700

    Move some functions to the fund class

---
+++ account.py
@@ -0,0 +1,54 @@
+# Created by Lucas Stoltman
+# Program 6
+# May 19th, 2022
+# CSS 340 A, Dimpsey
+# Version: 1.0
+
+class Account:
+    import string
+
+    # Constructor
+    def __init__(self,
+                 account_number: int = 1000,
+                 first_name: string = "Alan",
+                 last_name: string = "Turing"):
+        self._number = account_number
+        self._first_name = first_name
+        self._last_name = last_name
+
+    # Setters/Getters
+    def get_number(self):
+        return self._number
+
+    def set_number(self, value: int):
+        self._number = value
+        return True
+
+    def get_first_name(self):
+        return self._first_name
+
+    def set_first_name(self, new_name: string):
+        self._first_name = new_name
+        return True
+
+    def get_last_name(self):
+        return self._last_name
+
+    def set_last_name(self, new_name: string):
+        self._last_name = new_name
+        return True
+
+    # functions
+    def display(self):
+        print(self._number, self._first_name, self._last_name)
+
+    # string overload
+    def __str__(self):
+        result = f"{self._number} " \
+                 f"{self._first_name} {self._last_name}"
+        return result
+
+    # repr overload
+    def __repr__(self):
+        return "{self.__class__.__name__}({self._number}, " \
+               "{self._first_name}, {self._last_name})".format(self=self)
