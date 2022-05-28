5cdef9ccf8fca8688344e61cdc3018c48c2598d7
RainyKnight <stoltmanlucas@gmail.com>
2022-05-27 23:52:09 -0700

    Add basic file reading

--- command_reader.py
+++ command_reader.py
@@ -1,3 +1,13 @@
 # TODO read input from file
 
 class CommandReader:
+
+    _input = ""
+
+    def __init__(self, ):
+        self._input = open("BankTransIn.txt", "r")
+
+    def print(self):
+        print(self._input)
+
+
