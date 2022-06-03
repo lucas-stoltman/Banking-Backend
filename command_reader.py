3a830684d09954525da61e5531f8637e866080c7
RainyKnight <stoltmanlucas@gmail.com>
2022-06-03 12:32:12 -0700

    Clean newline characters

--- command_reader.py
+++ command_reader.py
@@ -2,12 +2,22 @@
 
 class CommandReader:
 
-    _input = ""
+    import queue
+
+    _file = open("BankTransIn.txt", "r")
+    _lines_list = [""]
 
     def __init__(self):
-        self._input = open("BankTransIn.txt", "r")
+        self._file = open("BankTransIn.txt", "r")
+        self._lines_list = self._file.readlines()
+        self.clean()
+
+    # remove newline characters
+    def clean(self):
+        for x in range(len(self._lines_list)):
+            self._lines_list[x] = self._lines_list[x].strip("\n")
 
     def print(self):
-        print(self._input.read())
+        print(self._lines_list)
 
 
